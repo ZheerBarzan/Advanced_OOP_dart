e32793504f0ae9d76a6a702430e1fb947cf10541
zheer barzan <zheerbarzan85@outlook.com>
2024-01-12 22:27:25 +0300

    adding the 3rd question

---
+++ Lab_1/Question_3/main.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  // guassian problem
+  print("enter the first number:");
+  double? firstNum = double.parse(stdin.readLineSync()!);
+
+  print("enter the second number:");
+  double? secondNum = double.parse(stdin.readLineSync()!);
+
+
+
+
+}
