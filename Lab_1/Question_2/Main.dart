5f7de47d2e93b8940a23cfe6e1398da366b2c9f3
zheer barzan <zheerbarzan85@outlook.com>
2024-01-12 22:21:42 +0300

    solving the guassian problem

--- Lab_1/Question_2/Main.dart
+++ Lab_1/Question_2/Main.dart
@@ -1,3 +1,14 @@
-void main(){
-  print("hello from the other side");
+import 'dart:io';
+
+void main() {
+  // guassian problem
+  print("enter the first number:");
+  double? firstNum = double.parse(stdin.readLineSync()!);
+
+
+  print("enter the second number:");
+  double? secondNum = double.parse(stdin.readLineSync()!);
+
+
+
 }
