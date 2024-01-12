631b252c4ca7b384e606ddec072c63a26bdf80fa
zheer barzan <zheerbarzan85@outlook.com>
2024-01-12 21:16:40 +0300

    adding the proper variables for the code

--- Lab_1/Question_1/Main.dart
+++ Lab_1/Question_1/Main.dart
@@ -1,3 +1,19 @@
+import 'dart:io';
+
+
+
 void main(){
-  print("hello");
+
+  print("enter the hight:");
+  int hight = stdin.readLineSync() as int;
+  print("enter the base:");
+  int base = stdin.readLineSync() as int;
+
+
+
+
+  print("the area of a tringle is:");
+
+
+
 }
