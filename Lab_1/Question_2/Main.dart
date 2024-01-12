739414ea0a9fecdc5e33081382c35d395da73911
zheer barzan <zheerbarzan85@outlook.com>
2024-01-12 22:25:12 +0300

    finishig the soulution

--- Lab_1/Question_2/Main.dart
+++ Lab_1/Question_2/Main.dart
@@ -10,5 +10,8 @@ void main() {
   double? secondNum = double.parse(stdin.readLineSync()!);
 
 
+  double result = (secondNum *(firstNum+secondNum))/2;
+
+  print("the sum of numbers form ${firstNum} to ${secondNum} is equal: ${result}");
 
 }
