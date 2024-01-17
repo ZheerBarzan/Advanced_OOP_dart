d68b9cfb9df6b21df0677b3ce650d348fda8c576
zheer barzan <zheerbarzan85@outlook.com>
2024-01-17 23:14:34 +0300

    adding the necessary variables and constroucr

---
+++ Lab_3/Student.dart
@@ -0,0 +1,11 @@
+import 'Course.dart';
+
+class Student {
+  int id;
+  String name;
+  List<Course> module;
+  double average;
+  String program;
+
+  Student(this.id, this.name, this.module, this.average, this.program);
+}
