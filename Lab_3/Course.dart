c963ad73e75660d54277ef7b8c4b3b4aa729943a
zheer barzan <zheerbarzan85@outlook.com>
2024-01-17 23:10:49 +0300

    adding the neccessary variables for course class

---
+++ Lab_3/Course.dart
@@ -0,0 +1,13 @@
+class Course {
+   String title;
+   String day;
+   String time;
+   String venue;
+   double mark;
+
+   Course(this.title, this.day, this.time, this.venue, this.mark) {
+     throw UnimplementedError();
+   }
+
+
+}
