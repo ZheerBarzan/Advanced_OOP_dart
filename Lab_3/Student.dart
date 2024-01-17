d9d1c6178bfa030133ba524dee5c882c9d87402f
zheer barzan <zheerbarzan85@outlook.com>
2024-01-18 01:30:09 +0300

    adding getter and setter for the student class

--- Lab_3/Student.dart
+++ Lab_3/Student.dart
@@ -1,14 +1,41 @@
 import 'Course.dart';
 
 class Student {
-  int id;
-  String name;
-  List<Course> module;
-  double average;
-  String program;
+  int _id;
+  String _name;
+  List<Course> _module;
+  double _average;
+  String _program;
 
-  Student(this.id, this.name, this.module, this.average, this.program) {
-    // TODO: implement Student
-    throw UnimplementedError();
+  Student(this._id, this._name, this._module, this._average, this._program);
+
+  String get program => _program;
+
+  set program(String value) {
+    _program = value;
+  }
+
+  double get average => _average;
+
+  set average(double value) {
+    _average = value;
+  }
+
+  List<Course> get module => _module;
+
+  set module(List<Course> value) {
+    _module = value;
+  }
+
+  String get name => _name;
+
+  set name(String value) {
+    _name = value;
+  }
+
+  int get id => _id;
+
+  set id(int value) {
+    _id = value;
   }
 }
