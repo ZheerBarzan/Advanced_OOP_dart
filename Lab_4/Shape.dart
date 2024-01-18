7e7307432fc59dd5c3940cca70b722a7fd656faa
zheer barzan <zheerbarzan85@outlook.com>
2024-01-19 01:08:34 +0300

    making the abstract class

---
+++ Lab_4/Shape.dart
@@ -0,0 +1,35 @@
+abstract class Shape{
+   String _color;
+   bool _filled;
+
+   static int count =0;
+
+
+   Shape(this._color, this._filled) {
+     count++;
+     throw UnimplementedError();
+   }
+
+  bool get filled => _filled;
+
+  set filled(bool value) {
+    _filled = value;
+  }
+
+  String get color => _color;
+
+  set color(String value) {
+    _color = value;
+  }
+
+  double getArea(){
+    return 0;
+  }
+  double getParamieter(){
+    return 0;
+  }
+   @override
+  String toString() {
+    return 'Shape{_color: $_color, _filled: $_filled}';
+  }
+}
