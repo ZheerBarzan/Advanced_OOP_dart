1e4afa296e52babad2bfc0b146e6174890e85809
zheer barzan <zheerbarzan85@outlook.com>
2024-01-20 02:00:36 +0300

    adding the Circle class

---
+++ Lab_4/Circle.dart
@@ -0,0 +1,30 @@
+import 'Shape.dart';
+
+class Circle extends Shape{
+  double _radius;
+
+  Circle(this._radius,super.color, super.filled);
+
+
+  double get radius => _radius;
+
+  set radius(double value) {
+    _radius = value;
+  }
+
+  @override
+  double getArea() {
+    // TODO: implement getArea
+    return super.getArea();
+  }
+  @override
+  double getParamieter() {
+    // TODO: implement getParamieter
+    return super.getParamieter();
+  }
+
+  @override
+  String toString() {
+    return 'Rectangle{_radius: $radius , filled: ${super.filled} , color: ${super.color}';
+  }
+}
